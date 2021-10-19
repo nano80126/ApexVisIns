db4fdd58412c71532362434e4ed217b2efc86f72
Ke LiDuan <nano80126@gmail.com>
2021-10-19 23:48:22 +0800

    Change scrollviewer style

--- content/DebugTab.xaml.cs
+++ content/DebugTab.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,16 +19,92 @@ namespace ApexVisIns.content
     /// <summary>
     /// Programming.xaml 的互動邏輯
     /// </summary>
-    public partial class DebugTab : StackPanel
+    public partial class DebugTab : StackPanel, INotifyPropertyChanged
     {
         public DebugTab()
         {
             InitializeComponent();
         }
 
+
+        public double ZoomRatio
+        {
+            get => ImageViewbox.Width / ImageCanvas.Width * 100;
+            set
+            {
+                int v = (int)Math.Floor(value);
+
+                if (20 > v)
+                {
+                    ImageViewbox.Width = 0.2 * ImageCanvas.Width;
+                }
+                else if (v > 200)
+                {
+                    ImageViewbox.Width = 2 * ImageCanvas.Width;
+                }
+                else
+                {
+                    double ratio = value / 100;
+                    ImageViewbox.Width = ratio * ImageCanvas.Width;
+                }
+                OnPropertyChanged(nameof(ZoomRatio));
+            }
+        }
+
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void ImageScroller_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            ScrollViewer viewer = sender as ScrollViewer;
+            //System.Windows.Point pt = e.GetPosition(ImageCanvas);
+
+            if (Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl))
+            {
+                if (e.Delta > 0)
+                {
+                    ZoomRatio += 5;
+                }
+                else
+                {
+                    ZoomRatio -= 5;
+                }
+            }
+
+
+
+
+        }
+
+        private void ImageCanvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void ImageCanvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void ImageCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void ImageCanvas_MouseLeave(object sender, MouseEventArgs e)
+        {
+
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
