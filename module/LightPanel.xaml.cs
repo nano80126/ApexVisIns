171ab07f7bebb7a9b0170070ba1670016a05fe58
Ke LiDuan <nano80126@gmail.com>
2021-11-08 15:43:47 +0800

    Add light panel module and wait for adding longworker

---
+++ module/LightPanel.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+
+
+namespace ApexVisIns.module
+{
+    /// <summary>
+    /// LightPanel.xaml 的互動邏輯
+    /// </summary>
+    public partial class LightPanel : Card
+    {
+        public LightPanel()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void ComPortSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComPortConnect_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
